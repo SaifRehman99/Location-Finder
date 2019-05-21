00a4bb75382f32b94ba541fd48f05890f13d9ed6
SaifRehman99 <saifr7493@gmail.com>
2019-05-21 16:46:45 +0500

    adding Javascript

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+// getting the references here
+const resDiv = document.querySelector('#output');
+const inputText = document,querySelector('#locationSearch');
+
+
+// adding event listener here
+inputText
