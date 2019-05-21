1a8a0ffc3dbfb49e727367f32a7299b9b5893dbf
SaifRehman99 <saifr7493@gmail.com>
2019-05-21 18:20:01 +0500

    Finalizing

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,7 @@ const resDiv = document.querySelector('#output');
 const inputText = document.querySelector('#locationSearch');
 
 
+
 // adding event listener here
 inputText.addEventListener('input', () => findLocation(inputText.value));
 
