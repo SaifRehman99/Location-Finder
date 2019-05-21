3d26680f5e5418e7d0c227780f5b69b83a3eb181
SaifRehman99 <saifr7493@gmail.com>
2019-05-21 17:25:30 +0500

    adding Higher order function

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,35 @@
 // getting the references here
 const resDiv = document.querySelector('#output');
-const inputText = document,querySelector('#locationSearch');
+const inputText = document.querySelector('#locationSearch');
 
 
 // adding event listener here
-inputText
+inputText.addEventListener('input',()=> findLocation(inputText.value));
+
+// defining the async function here to fetch the data
+let findLocation = async (value)=>{
+
+    // fetching the json data here
+    let jsonData = await fetch('JSON Data/location.js');
+
+    // data here
+    let data = await jsonData.json();
+
+    // getting the required data
+    let matchData = data.filter((item)=>{
+        
+        // using regx here to validate the search
+
+        // ^ used to when search item it search for the first letter, g for globally and i for case insensitive
+        let regx = new RegExp(`^${value}`, `gi`);
+        
+        return item.name.match(regx);
+    });
+
+    // validation here
+    if(value.length === 0){
+         matchData = [];
+    }
+
+    console.log(matchData)
+};
